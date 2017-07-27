be546d359bb0be622343bd76b4ac7821f9a1fafb
luguosheng101 <lu.guosheng@eisoo.com>
2017-07-27 17:47:47 +0800

    lu

---
+++ js/checkboxFunction.js
@@ -0,0 +1,24 @@
+﻿function checkAll() {
+    for (var i = 0; i < document.getElementById("cblStatuteEnumTypeId").getElementsByTagName("input").length; i++) {
+        document.getElementById("cblStatuteEnumTypeId_" + i).checked = true;
+    }
+}
+function deleteAll() {
+
+    for (var i = 0; i < document.getElementById("cblStatuteEnumTypeId").getElementsByTagName("input").length; i++) {
+
+        document.getElementById("cblStatuteEnumTypeId_" + i).checked = false;
+    }
+}
+function ReverseAll() {
+
+    for (var i = 0; i < document.getElementById("CheckBoxList1").getElementsByTagName("input").length; i++) {
+        var objCheck = document.getElementById("CheckBoxList1_" + i);
+
+        if (objCheck.checked)
+
+            objCheck.checked = false;
+        else
+            objCheck.checked = true;
+    }
+}
